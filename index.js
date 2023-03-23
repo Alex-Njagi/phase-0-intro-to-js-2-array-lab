3a7cf629fb6e1102d5ca5ad50bae45637d838d53
Alex Njagi <1992anjagi@gmail.com>
2023-03-23 08:59:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
-
-cats.splice(start=2, deleteCount= 1, items[0]="Ralph");
-console.log(data[0]='cats');
+let cats = [`Milo`, `Otis`, `Garfield`];
+//--------------------------------------------------------------------------------
+function destructivelyAppendCat(cats){
+    return cats.splice([3],[0],`Ralph`);
+}
+destructivelyAppendCat(cats=cats);
+cats
+//---------------------------------------------------------------------------------
+function destructivelyPrependCat(cats){
+    return cats.splice([0],[0],`Spencer`);
+}
+destructivelyPrependCat(cats=cats);
+cats
+//---------------------------------------------------------------------------------
+function destructivelyRemoveLastCat() {
+    return cats.splice([4],[1]);
+}
+destructivelyRemoveLastCat();
+cats
+//----------------------------------------------------------------------------------
+function destructivelyRemoveFirstCat() {
+    return cats.splice([0],[1]);
+}
+destructivelyRemoveFirstCat();
+cats
+//----------------------------------------------------------------------------------
+const prependedCat = [...cats];
+function prependCat() {
+    return prependedCat.unshift(`Spencer`)
+}
+prependCat(prependedCat);
+cats
+prependedCat
+//----------------------------------------------------------------------------------
+const appendedCat = [...cats];
+function appendCat() {
+    return appendedCat.push(`Alison`)
+}
+appendCat(appendedCat);
+cats
+appendedCat
+//----------------------------------------------------------------------------------
+const removedLastCat = [...cats];
+function removeLastCat() {
+    return removedLastCat.pop()
+}
+removeLastCat(removedLastCat);
+cats
+removedLastCat
+//----------------------------------------------------------------------------------
+const removedFirstCat = [...cats];
+function removeFirstCat() {
+    return removedFirstCat.shift();
+}
+removeFirstCat(removedFirstCat);
+cats
+removedFirstCat
